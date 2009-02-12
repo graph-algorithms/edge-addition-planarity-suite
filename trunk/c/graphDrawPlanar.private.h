840e8c71d09430cd9fb8da999a3b7d1e5c1befb7
boyerj@ca.ibm.com <boyerj@ca.ibm.com@45cb6f38-8da3-11dd-aa02-45bbecb07bf0>
2009-02-12 07:09:16 +0000

    Applied an updated BSD license to all files (except nauty, which has its own license and which can be excluded from builds)

--- trunk/c/graphDrawPlanar.private.h
+++ trunk/c/graphDrawPlanar.private.h
@@ -1,9 +1,49 @@
 #ifndef GRAPH_DRAWPLANAR_PRIVATE_H
 #define GRAPH_DRAWPLANAR_PRIVATE_H
 
-/* Copyright (c) 2008 by John M. Boyer, All Rights Reserved.
-        This code may not be reproduced or disseminated in whole or in part
-        without the written permission of the author. */
+/*
+Planarity-Related Graph Algorithms Project
+Copyright (c) 1997-2009, John M. Boyer
+All rights reserved. Includes a reference implementation of the following:
+
+* John M. Boyer. "Simplified O(n) Algorithms for Planar Graph Embedding,
+  Kuratowski Subgraph Isolation, and Related Problems". Ph.D. Dissertation,
+  University of Victoria, 2001.
+
+* John M. Boyer and Wendy J. Myrvold. "On the Cutting Edge: Simplified O(n)
+  Planarity by Edge Addition". Journal of Graph Algorithms and Applications,
+  Vol. 8, No. 3, pp. 241-273, 2004.
+
+* John M. Boyer. "A New Method for Efficiently Generating Planar Graph
+  Visibility Representations". In P. Eades and P. Healy, editors,
+  Proceedings of the 13th International Conference on Graph Drawing 2005,
+  Lecture Notes Comput. Sci., Volume 3843, pp. 508-511, Springer-Verlag, 2006.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this
+  list of conditions and the following disclaimer in the documentation and/or
+  other materials provided with the distribution.
+
+* Neither the name of the Planarity-Related Graph Algorithms Project nor the names
+  of its contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #include "graph.h"
 
@@ -13,8 +53,8 @@ extern "C" {
 
 // Additional equipment for each graph node (edge arc or vertex)
 /*
-        pos, start, end: used to store a visibility representation, or 
-                horvert diagram of a planar graph.  
+        pos, start, end: used to store a visibility representation, or
+                horvert diagram of a planar graph.
                 For vertices, vertical position, horizontal range
                 For edges, horizontal position, vertical range
 */
@@ -27,14 +67,14 @@ typedef DrawPlanar_GraphNode * DrawPlanar_GraphNodeP;
 
 // Additional equipment for each vertex
 /*
-        drawingFlag, ancestor, ancestorChild: used to collect information needed 
-                to help 'draw' a visibility representation.  During planar 
-                embedding, a vertex is determined to be between its DFS parent and 
-                a given ancestor (the vertex being processed) or beyond the parent 
-                relative to the ancestor. In post processing, the relative 
-                orientation of the parent and ancestor are determined, 
-                then the notion of between/beyond resolves to above/below or 
-                below/above depending on whether the ancestor is above or below, 
+        drawingFlag, ancestor, ancestorChild: used to collect information needed
+                to help 'draw' a visibility representation.  During planar
+                embedding, a vertex is determined to be between its DFS parent and
+                a given ancestor (the vertex being processed) or beyond the parent
+                relative to the ancestor. In post processing, the relative
+                orientation of the parent and ancestor are determined,
+                then the notion of between/beyond resolves to above/below or
+                below/above depending on whether the ancestor is above or below,
                 respectively, the parent.  The ancestorChild are used to help r
                 esolve this latter question.
         tie[2]  stores information along the external face during embedding
@@ -71,7 +111,7 @@ typedef struct
     DrawPlanar_GraphNodeP G;
 
     // Parallel array for additional vertex level equipment
-    DrawPlanar_VertexRecP V;    
+    DrawPlanar_VertexRecP V;
 
     // Overloaded function pointers
     graphFunctionTable functions;
