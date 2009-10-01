d3b5fd91c7bf67ce6ff6243cc8cf504268e659b6
boyerj@ca.ibm.com <boyerj@ca.ibm.com@45cb6f38-8da3-11dd-aa02-45bbecb07bf0>
2009-10-01 15:38:48 +0000

    Additional vertex and edge information needed for K4 isolation, with comments explaining additional processing.

--- trunk/c/graphK4Search.private.h
+++ trunk/c/graphK4Search.private.h
@@ -15,23 +15,61 @@ the expressed written permission of the copyright holder.
 extern "C" {
 #endif
 
-// Additional equipment for each graph node (edge arc or vertex)
+/* Additional equipment for each graph node (edge arc or vertex)
+
+   pathConnector:
+      Used in the edge records (arcs) of a reduction edge to indicate the
+      endpoints of a path that has been reduced from (removed from) the
+      embedding so that the search for a K4 can continue.
+	  We only need a pathConnector because we reduce subgraphs that are
+	  separable by a 2-cut, so they can contribute at most one path to a
+	  subgraph homeomorphic to K4, if one is indeed found. Thus, we first
+	  delete all edges except for the desired path(s), then we reduce any
+	  retained path to an edge.
+
+   subtree:
+      Used in each forward arc (V, D) to indicate the DFS child C of V whose
+      DFS subtree contains the DFS descendant endpoint D of the forward arc.
+      This helps to efficiently find C when (V, D) is embedded so that the
+      p2dFwdArcCount of C can be decremented.
+      In order to efficiently calculate this value in preprocessing, the
+      fwdArcList of each vertex is sorted by descendant endpoint DFS number,
+      and the sortedDFSChildList of each vertex is computed. This enables
+      the subtree settings to be made with a single pass simultaneously
+      through the fwdArcList and sortedDFSChildList. See CreateFwdArcLists
+      for implementation details.
+ */
 typedef struct
 {
-	// We only need a pathConnector because we reduce subgraphs that
-	// are separable by a 2-cut, so they can contribute at most one
-	// path to a subgraph homeomorphic to K4, if one is indeed found.
-	// Thus, we first delete all edges except for the desired path(s),
-	// then we reduce any retained path to an edge.
-     int  pathConnector;
+     int  pathConnector, subtree;
 } K4Search_GraphNode;
 
 typedef K4Search_GraphNode * K4Search_GraphNodeP;
 
-// Additional equipment for each vertex
+/* Additional equipment for each vertex
+
+   p2dFwdArcCount:
+      During preprocessing, for each vertex we need to know how many forward arcs
+      there are from the DFS parent of the vertex to the DFS descendants of the
+      vertex. As each forward arc is embedded, we decrement this count. When this
+      count reaches zero, we remove the vertex from the sortedDFSChildList of its
+      parent.
+
+   sortedDFSChildList:
+      During preprocessing, we need a list of the DFS children for each vertex,
+      sorted by their DFS numbers. The core planarity/outerplanarity algorithm
+      calculates a separatedDFSChildList that is sorted by the children's Lowpoints.
+      During processing, a child is removed from the sortedDFSChildList of its
+      parent when the p2dFwdArcCount of the child reaches zero. Thus, this list
+      indicates which subtree children of a vertex still contain unresolved
+      pertinence (unembedded forward arcs). The main search for K4 homeomorphs
+      uses this list to efficiently determine the portion of the graph in which
+      to either find a K4 or to perform reductions than enable more forward arcs
+      to be embedded.
+*/
 typedef struct
 {
-        int sortedDFSChildList;
+        int p2dFwdArcCount, sortedDFSChildList;
 } K4Search_VertexRec;
 
 typedef K4Search_VertexRec * K4Search_VertexRecP;
