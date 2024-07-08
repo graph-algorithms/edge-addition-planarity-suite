919eff4d937f4ec3a7477c180f5df841d364856b
Wanda Boyer <wbkboyer@gmail.com>
2024-07-08 14:35:12 -0700

    Removing logging machinery

--- TestSupport/planaritytesting/leaksorchestrator/planarity_leaks_orchestrator.py
+++ TestSupport/planaritytesting/leaksorchestrator/planarity_leaks_orchestrator.py
@@ -7,7 +7,6 @@ __all__ = []
 import argparse
 from datetime import datetime
 import configparser
-import logging
 import multiprocessing
 import os
 from pathlib import Path
@@ -115,45 +114,6 @@ class PlanarityLeaksOrchestrator:
 
         self.planarity_path = planarity_path
         self.output_dir = output_dir
-
-        self._setup_logger()
-
-    def _setup_logger(self) -> None:
-        """Set up logger instance for PlanarityLeaksAnalyzer"""
-
-        log_path = Path.joinpath(
-            self.output_dir,
-            f"edge_deletion_analysis-{self.curr_timestamp}.log",
-        )
-        # TODO: set up logging support for multithreading
-
-        # logging.getLogger() returns the *same instance* of a logger
-        # when given the same name. In order to prevent this, must either give
-        # a unique name, or must prevent adding additional handlers to the
-        # logger
-        self.logger = logging.getLogger(__name__ + self.curr_timestamp)
-        self.logger.setLevel(logging.DEBUG)
-
-        if not self.logger.handlers:
-            # Create the Handler for logging data to a file
-            logger_handler = logging.FileHandler(filename=log_path)
-            logger_handler.setLevel(logging.DEBUG)
-
-            # Create a Formatter for formatting the log messages
-            logger_formatter = logging.Formatter(
-                "[%(levelname)s] - %(module)s.%(funcName)s - %(message)s"
-            )
-
-            # Add the Formatter to the Handler
-            logger_handler.setFormatter(logger_formatter)
-
-            # Add the Handler to the Logger
-            self.logger.addHandler(logger_handler)
-
-            stderr_handler = logging.StreamHandler()
-            stderr_handler.setLevel(logging.ERROR)
-            stderr_handler.setFormatter(logger_formatter)
-            self.logger.addHandler(stderr_handler)
 
     @staticmethod
     def setup_leaks_environment_variables(
