89ca75fa4a2f35357e4aed64ec837860f77f1878
Wanda Boyer <wbkboyer@gmail.com>
2024-10-04 09:40:41 -0700

    Issue #94 - Separate Transform Graph and Test All Graphs functionality (#101)
    Resolves #94 
    
    Confirmed on MacOS that:
    - command-line tests (-test) pass
    - Menu-driven transform and test all graphs work
    - command-line transform and test all graphs work
    - debugger works
    - no additional compiler warnings introduced
    
    * Minor tweaks to documentation comments

--- c/planarityApp/planarity.h
+++ c/planarityApp/planarity.h
@@ -30,7 +30,8 @@ extern "C"
         char *inputStr, char **pOutputStr, char **pOutput2Str);
     int RandomGraph(char command, int extraEdges, int numVertices, char *outfileName, char *outfile2Name);
     int RandomGraphs(char command, int NumGraphs, int SizeOfGraphs);
-    int TestGraphFunctionality(char *commandString, char *infileName, char *inputStr, int *outputBase, char *outfileName, char **outputStr);
+    int TransformGraph(char *commandString, char *infileName, char *inputStr, int *outputBase, char *outfileName, char **outputStr);
+    int TestAllGraphs(char *commandString, char *infileName, int *outputBase, char *outfileName, char **outputStr);
 
     /* Command line, Menu, and Configuration */
     int menu();
