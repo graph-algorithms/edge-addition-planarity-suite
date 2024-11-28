2a66d68ee758e3c2cb5427caf30ae1101cb3ca79
Wanda Boyer <wbkboyer@gmail.com>
2024-11-27 16:10:04 -0800

    Extrapolated fix recommended by @orlitzky and verified semantics with @john-boyer-phd (#119)
    Closes #40 and Closes #118

--- c/graphLib/extensionSystem/graphFunctionTable.h
+++ c/graphLib/extensionSystem/graphFunctionTable.h
@@ -8,53 +8,56 @@ See the LICENSE.TXT file for licensing information.
 */
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-/*
- NOTE: If you add any FUNCTION POINTERS to this function table, then you must
-       also initialize them in _InitFunctionTable() in graphUtils.c.
-*/
+    /*
+     NOTE: If you add any FUNCTION POINTERS to this function table, then you must
+           also initialize them in _InitFunctionTable() in graphUtils.c.
+    */
+    typedef struct baseGraphStructure baseGraphStructure;
+    typedef baseGraphStructure *graphP;
 
-typedef struct
-{
+    typedef struct
+    {
         // These function pointers allow extension modules to overload some of
         // the behaviors of protected functions.  Only advanced applications
         // will overload these functions
-    	int  (*fpEmbeddingInitialize)();
-        void (*fpEmbedBackEdgeToDescendant)();
-        void (*fpWalkUp)();
-        int  (*fpWalkDown)();
-        int  (*fpMergeBicomps)();
-        void (*fpMergeVertex)();
-        int  (*fpHandleInactiveVertex)();
-        int  (*fpHandleBlockedBicomp)();
-        int  (*fpEmbedPostprocess)();
-        int  (*fpMarkDFSPath)();
+        int (*fpEmbeddingInitialize)(graphP);
+        void (*fpEmbedBackEdgeToDescendant)(graphP, int, int, int, int);
+        void (*fpWalkUp)(graphP, int, int);
+        int (*fpWalkDown)(graphP, int, int);
+        int (*fpMergeBicomps)(graphP, int, int, int, int);
+        void (*fpMergeVertex)(graphP, int, int, int);
+        int (*fpHandleInactiveVertex)(graphP, int, int *, int *);
+        int (*fpHandleBlockedBicomp)(graphP, int, int, int);
+        int (*fpEmbedPostprocess)(graphP, int, int);
+        int (*fpMarkDFSPath)(graphP, int, int);
 
-        int  (*fpCheckEmbeddingIntegrity)();
-        int  (*fpCheckObstructionIntegrity)();
+        int (*fpCheckEmbeddingIntegrity)(graphP, graphP);
+        int (*fpCheckObstructionIntegrity)(graphP, graphP);
 
         // These function pointers allow extension modules to overload some
         // of the behaviors of gp_* function in the public API
-        int  (*fpInitGraph)();
-        void (*fpReinitializeGraph)();
-        int  (*fpEnsureArcCapacity)();
-        int  (*fpSortVertices)();
+        int (*fpInitGraph)(graphP, int);
+        void (*fpReinitializeGraph)(graphP);
+        int (*fpEnsureArcCapacity)(graphP, int);
+        int (*fpSortVertices)(graphP);
 
-        int  (*fpReadPostprocess)();
-        int  (*fpWritePostprocess)();
+        int (*fpReadPostprocess)(graphP, void *, long);
+        int (*fpWritePostprocess)(graphP, void **, long *);
 
-        void (*fpHideEdge)();
-        void (*fpRestoreEdge)();
-        int  (*fpHideVertex)();
-        int  (*fpRestoreVertex)();
-        int  (*fpContractEdge)();
-        int  (*fpIdentifyVertices)();
+        void (*fpHideEdge)(graphP, int);
+        void (*fpRestoreEdge)(graphP, int);
+        int (*fpHideVertex)(graphP, int);
+        int (*fpRestoreVertex)(graphP);
+        int (*fpContractEdge)(graphP, int);
+        int (*fpIdentifyVertices)(graphP, int, int, int);
 
-} graphFunctionTable;
+    } graphFunctionTable;
 
-typedef graphFunctionTable * graphFunctionTableP;
+    typedef graphFunctionTable *graphFunctionTableP;
 
 #ifdef __cplusplus
 }
