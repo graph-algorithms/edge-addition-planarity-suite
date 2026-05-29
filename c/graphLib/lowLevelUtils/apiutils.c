bd22b51d7c93ce68658ba85da5de1257dbb23aec
Wanda B.K. Boyer <wbkboyer@gmail.com>
2026-05-29 15:23:33 -0700

    `graphLib` casts "zone of silence"
    Consuming applications must set the appropriate flag:
    
    1.  if `gp_SetQuietMode(QUIETMODE_NONE)` is used, then we will display both `gp_Message()` and `gp_ErrorMessage()`
    2.  if `gp_SetQuietMode(QUIETMODE_ERRORS)` is used, then only `gp_Message()` will be displayed
    3.  if `gp_SetQuietMode(QUIETMODE_MESSAGES)` is used, then only `gp_ErrorMessage()` will be displayed
    4.  if `gp_SetQuietMode(QUIETMODE_ALL)` is used (the default for `graphLib`), then neither `gp_Message()` nor `gp_ErrorMessage()` will be shown

--- c/graphLib/lowLevelUtils/apiutils.c
+++ c/graphLib/lowLevelUtils/apiutils.c
@@ -15,7 +15,7 @@ See the LICENSE.TXT file for licensing information.
 
 // The graphLib gp_ErrorMessage() and gp_Message() calls are suppressed by
 // default, but an application can turn them on if desired.
-unsigned quietMode = TRUE;
+unsigned quietMode = QUITEMODE_ALL;
 
 unsigned gp_GetQuietMode(void)
 {
