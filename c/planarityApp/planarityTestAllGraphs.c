a007abacdf522008bcc745c0f4d7efed935b5007
john-boyer-phd <john.boyer.phd@gmail.com>
2026-06-02 12:26:55 -0700

    Alternative test all graphs code that can be used for performance testing.

--- c/planarityApp/planarityTestAllGraphs.c
+++ c/planarityApp/planarityTestAllGraphs.c
@@ -20,6 +20,12 @@ typedef testAllStats *testAllStatsP;
 int testAllGraphs(char command, char modifier, char const *const infileName, testAllStatsP stats);
 int outputTestAllGraphsResults(char command, char modifier, testAllStatsP stats, char const *const infileName, char *outfileName, char **pOutputStr);
 
+#define TESTALLGRAPHS_MEMORY_TIMING_TEST
+
+#ifdef TESTALLGRAPHS_MEMORY_TIMING_TEST
+int testAllGraphsN8(char command, char modifier, char const *const infileName, testAllStatsP stats);
+#endif
+
 /****************************************************************************
  TestAllGraphs()
  commandString - command to run; e.g.`-(pdo234)` (plus optional modifier
@@ -59,7 +65,11 @@ int TestAllGraphs(char const *const commandString, char const *const infileName,
     // Start the timer
     platform_GetTime(start);
 
+#ifndef TESTALLGRAPHS_MEMORY_TIMING_TEST
     Result = testAllGraphs(command, modifier, infileName, &stats);
+#else
+    Result = testAllGraphsN8(command, modifier, infileName, &stats);
+#endif
 
     // Stop the timer
     platform_GetTime(end);
@@ -326,3 +336,195 @@ int outputTestAllGraphsResults(char command, char modifier, testAllStatsP stats,
 
     return Result;
 }
+
+#ifdef TESTALLGRAPHS_MEMORY_TIMING_TEST
+
+/*******************************************************************************
+ * An alternative version of testAllGraphs() that can be used for timing tests
+ *******************************************************************************/
+
+#define NUM8VERTEXTGRAPHS 12346
+
+int testAllGraphsN8(char command, char modifier, char const *const infileName, testAllStatsP stats)
+{
+    int Result = OK;
+
+    graphP origGraphRead[NUM8VERTEXTGRAPHS + 1];
+    graphP graphForEmbedding = NULL;
+    int embedFlags = 0, numOK = 0, numNONEMBEDDABLE = 0;
+    int order = 0;
+    int lineNum = 0;
+    platform_time start, end;
+
+    G6ReadIteratorP theG6ReadIterator = NULL;
+
+    if (infileName == NULL || !strstr(infileName, "n8.mALL.g6"))
+    {
+        gp_ErrorMessage("testAllGraphsN8() is coded just for processing all 8-vertex graphs");
+        return NOTOK;
+    }
+
+    if (GetEmbedFlags(command, modifier, &embedFlags) != OK)
+    {
+        gp_ErrorMessage("Invalid command or modifier.\n");
+        stats->errorFlag = TRUE;
+        return NOTOK;
+    }
+
+    // Initialize all pointer and allocate a graph structure that is
+    // needed to read all graphs from the file into memory
+    for (lineNum = 0; lineNum <= NUM8VERTEXTGRAPHS; lineNum++)
+        origGraphRead[lineNum] = NULL;
+
+    if ((origGraphRead[0] = gp_New()) == NULL)
+    {
+        gp_ErrorMessage("Unable to allocate graph for reading.\n");
+        stats->errorFlag = TRUE;
+        return NOTOK;
+    }
+
+    // The 0th origGraph will be used by the read iterator, and then the
+    // graphs will be copied into locations 1 through NUM8VERTEXTGRAPHS
+    if (g6_NewReader((&theG6ReadIterator), origGraphRead[0]) != OK ||
+        g6_InitReaderWithFileName(theG6ReadIterator, infileName) != OK)
+    {
+        gp_ErrorMessage("Unable to allocate or initialize G6 read iterator.\n");
+        gp_Free(&origGraphRead[0]);
+        g6_FreeReader((&theG6ReadIterator));
+        stats->errorFlag = TRUE;
+        return NOTOK;
+    }
+
+    // The order of the graphs in the G6 source file or string was determined by
+    // g6_InitReaderWithFileName(), and we obtain it to ensure vertex capacity in
+    // the graphs that will receive copies origGraphRead[0] from each line of the
+    // g6 file and also to help initialize the graph for embedding
+    order = gp_GetN(origGraphRead[0]);
+
+    for (lineNum = 1; lineNum <= NUM8VERTEXTGRAPHS; lineNum++)
+    {
+        if ((origGraphRead[lineNum] = gp_New()) == NULL ||
+            gp_EnsureVertexCapacity(origGraphRead[lineNum], order) != OK)
+        {
+            gp_ErrorMessage("Unable allocate graphs for reading.\n");
+            g6_FreeReader((&theG6ReadIterator));
+            for (lineNum = 0; lineNum <= NUM8VERTEXTGRAPHS; lineNum++)
+                gp_Free(&origGraphRead[lineNum]);
+            stats->errorFlag = TRUE;
+            return NOTOK;
+        }
+    }
+
+    if ((graphForEmbedding = gp_New()) == NULL ||
+        gp_EnsureVertexCapacity(graphForEmbedding, order) != OK ||
+        ExtendGraph(graphForEmbedding, command) != OK)
+    {
+        gp_ErrorMessage("Unable allocate graph for embedding.\n");
+        g6_FreeReader((&theG6ReadIterator));
+        for (lineNum = 0; lineNum <= NUM8VERTEXTGRAPHS; lineNum++)
+            gp_Free(&origGraphRead[lineNum]);
+        gp_Free(&graphForEmbedding);
+        stats->errorFlag = TRUE;
+        return NOTOK;
+    }
+
+    // Read the graphs into memory
+    lineNum = 0;
+    while (lineNum < NUM8VERTEXTGRAPHS)
+    {
+        lineNum++;
+        if (g6_ReadGraph(theG6ReadIterator) != OK)
+        {
+            gp_ErrorMessage("Unable to read graph on line %d.\n", lineNum);
+            Result = NOTOK;
+            break;
+        }
+
+        if (g6_EndReached(theG6ReadIterator))
+            break;
+
+        if (gp_CopyGraph(origGraphRead[lineNum], origGraphRead[0]) != OK)
+        {
+            gp_ErrorMessage("Unable to copy graph.\n");
+            Result = NOTOK;
+            break;
+        }
+    }
+
+    g6_FreeReader((&theG6ReadIterator));
+
+    if (Result != OK)
+    {
+        for (lineNum = 0; lineNum <= NUM8VERTEXTGRAPHS; lineNum++)
+            gp_Free(&origGraphRead[lineNum]);
+        gp_Free(&graphForEmbedding);
+        return NOTOK;
+    }
+
+    // Since we increment lineNum at the beginning of the loop, if an error
+    // occurs during processing a graph on the current lineNum, or if we reach
+    // the end of the input, then the number of graphs successfully read is
+    // lineNum - 1.
+    stats->numGraphsTested = lineNum - 1;
+
+    // Start a timer, and we will iterate the work many times to
+    // get a more reliable reading.
+
+    platform_GetTime(start);
+
+    for (int i = 0; i < 1000; ++i)
+    {
+        numOK = numNONEMBEDDABLE = 0;
+        for (lineNum = 1; lineNum <= NUM8VERTEXTGRAPHS; lineNum++)
+        {
+            if (gp_CopyGraph(graphForEmbedding, origGraphRead[lineNum]) != OK)
+            {
+                gp_ErrorMessage("Unable to copy graph.\n");
+                Result = NOTOK;
+                break;
+            }
+
+            Result = gp_Embed(graphForEmbedding, embedFlags);
+            if (Result != OK && Result != NONEMBEDDABLE)
+            {
+                gp_ErrorMessage("Failed to embed graph on line %d", lineNum);
+                Result = NOTOK;
+                break;
+            }
+
+            if (gp_TestEmbedResultIntegrity(graphForEmbedding, origGraphRead[lineNum], Result) != Result)
+            {
+                gp_ErrorMessage("Embed integrity check failed for graph on line %d", lineNum);
+                Result = NOTOK;
+                break;
+            }
+
+            if (Result == OK)
+                numOK++;
+            else if (Result == NONEMBEDDABLE)
+            {
+                numNONEMBEDDABLE++;
+                Result = OK;
+            }
+        }
+
+        if (Result != OK)
+            break;
+    }
+
+    platform_GetTime(end);
+    gp_Message("In-memory graph processing test executed in %.3lf seconds.\n",
+               platform_GetDuration(start, end));
+
+    stats->numOK = numOK;
+    stats->numNONEMBEDDABLE = numNONEMBEDDABLE;
+    stats->errorFlag = (Result == OK) ? FALSE : TRUE;
+
+    for (lineNum = 0; lineNum <= NUM8VERTEXTGRAPHS; lineNum++)
+        gp_Free(&origGraphRead[lineNum]);
+    gp_Free(&graphForEmbedding);
+
+    return Result == OK ? OK : NOTOK;
+}
+
+#endif
